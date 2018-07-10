12ca55bb7c7a06836087e0bcbffce82415e18fc3
seankdecker <seankdecker@gmail.com>
2018-07-10 13:21:22 -0700

    finished this up. Moving on/

--- qtf.py
+++ qtf.py
@@ -21,25 +21,16 @@ def exampleQft3(q0, q1, q2):
             CPHASE(pi/4.0, q0, q2),
             CPHASE(pi/2.0, q0, q1),
             H(q0),
-            SWAP(q0, q2) )
+            SWAP(q0, q2) ) # SWAP is necessary because the QTF swaps the qubits
     return p
 
-def qft3(q0, q1, q2):
-	p = Program()
-	p.inst(H(q2),
-		   CPHASE(pi/2.0, q1, q2), 
-		   H(q1),
-		   CPHASE(pi/4.0, q0, q2), CPHASE(pi/2.0, q0, q1),
-		   H(q0))
-	return p
-
 if __name__ == "__main__":
-	print('hi')
+	print('hi there, we will qtf [0, 1, 0, 0, 0, 0, 0, 0] \n')
 	qvm = QVMConnection()
 	state_prep = Program().inst(X(0), I(1), I(2))
 	# wavefunction = qvm.wavefunction(state_prep)
 	# print(wavefunction)
 
-	wavefunction = qvm.wavefunction(state_prep + qft3(0, 1, 2))
+	wavefunction = qvm.wavefunction(state_prep + exampleQft3(0, 1, 2))
 	print(wavefunction.amplitudes)
 	ifft([0, 1, 0, 0, 0, 0, 0, 0], norm="ortho")
